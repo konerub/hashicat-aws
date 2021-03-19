dd1138cdf956a1ba15f769f71cd432e296c3eba5
konerub <39737028+konerub@users.noreply.github.com>
2021-03-19 15:54:53 -0400

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  CHANGED IN GITHUB-1
+  CHANGED IN GITHUB - 1
   <!-- END -->
 
   </div>
